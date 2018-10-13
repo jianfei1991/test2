69c08eacbf10c4318aa5483f8225c67825665431
Wu Jianfei <wujianfei@shouqiev.com>
2018-10-13 19:18:16 +0800

    test

--- bak.js
+++ bak.js
@@ -4,6 +4,7 @@ node_js:
 after_script:
 - touch a.js
 before_install:
+- touch a.js
 - ls
 - openssl aes-256-cbc -K $encrypted_429b1cf1584d_key -iv $encrypted_429b1cf1584d_iv
-  -in travis_rsa.enc -out ~\/.ssh/travis_rsa -d
+  -in aliyun.enc -out ~\/.ssh/aliyun -d
